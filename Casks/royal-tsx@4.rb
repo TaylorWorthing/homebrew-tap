29783f8bf7571690d202e138dfc4f2691fa78811
Taylor Worthing <TaylorWorthing@users.noreply.github.com>
2022-03-11 16:06:48 -0600

    Add royal-tsx@4

---
+++ Casks/royal-tsx@4.rb
@@ -0,0 +1,26 @@
+cask "royal-tsx@4" do
+  version "4.3.6.1000"
+  sha256 "9fd00742126a19acca5b93751c57236093639c7c77955af6375592dce4e44360"
+
+  url "https://royaltsx-v4.royalapps.com/updates/royaltsx_#{version}.dmg"
+  name "Royal TSX"
+  desc "Remote management solution"
+  homepage "https://www.royalapps.com/ts/mac/features"
+
+  livecheck do
+    url "https://royaltsx-v#{version.major}.royalapps.com/updates_stable.php"
+    strategy :sparkle
+  end
+
+  auto_updates true
+  depends_on macos: ">= :sierra"
+
+  app "Royal TSX.app"
+
+  zap trash: [
+    "~/Library/Application Support/Royal TSX",
+    "~/Library/Application Support/com.lemonmojo.RoyalTSX.App",
+    "~/Library/Caches/com.lemonmojo.RoyalTSX.App",
+    "~/Library/Preferences/com.lemonmojo.RoyalTSX.App.plist",
+  ]
+end
